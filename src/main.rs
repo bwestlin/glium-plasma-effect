9b8fe155bc89331f6edc709c666eb43fd88a4e87
Björn Westlin <bwestlin@gmail.com>
2015-12-16 21:21:05 +0100

    A first try at rendering in multiple threads

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ extern crate glium;
 extern crate glium_sdl2;
 extern crate sdl2;
 extern crate time;
+extern crate scoped_threadpool;
 
 mod plasma;
 
 use glium_sdl2::DisplayBuild;
 use glium::Surface;
 use time::*;
+use std::thread;
 
 use plasma::Plasma;
 
@@ -28,17 +30,21 @@ fn main() {
 
     let b_width = 480 as u32;
     let b_height = 320 as u32;
-    let n_pixels = b_width * b_width;
+    let n_pixels = b_width * b_height;
     let mut e_buffer: Vec<(u8, u8, u8, u8)> = Vec::with_capacity(n_pixels as usize);
     e_buffer.resize(n_pixels as usize, (0u8, 0u8, 0u8, 0u8));
+    let e_buf_len = e_buffer.len();
 
-    let mut plasma = Plasma::new(b_width, b_height);
+    let plasma = &(Plasma::new(b_width, b_height));
 
     let texture = glium::texture::Texture2d::empty(&display, b_width, b_height).unwrap();
     let p_buffer = glium::texture::pixel_buffer::PixelBuffer::new_empty(&display, n_pixels as usize);
 
     let mut fps_counter = FpsCounter::new();
     let start_time_ns = precise_time_ns();
+
+    let n_chunks = 4;
+    let mut pool = scoped_threadpool::Pool::new(n_chunks);
 
     while running {
         fps_counter.frame_time(precise_time_ns());
@@ -47,7 +53,15 @@ fn main() {
         let mut target = display.draw();
         target.clear_color(0.0, 0.0, 0.0, 1.0);
 
-        plasma.render(&mut e_buffer, precise_time_ns() - start_time_ns);
+        let delta_t = precise_time_ns() - start_time_ns;
+
+        pool.scoped(|scope| {
+            for eb_slice in e_buffer.chunks_mut(e_buf_len / n_chunks as usize) {
+                plasma.render(eb_slice, delta_t, 0, b_height / n_chunks);
+            }
+        });
+
+        //plasma.render(&mut e_buffer, precise_time_ns() - start_time_ns);
         p_buffer.write(&e_buffer);
         texture.main_level().raw_upload_from_pixel_buffer(p_buffer.as_slice(), 0 .. b_width, 0 .. b_height, 0 .. 1);
         texture.as_surface().fill(&target, glium::uniforms::MagnifySamplerFilter::Linear);
